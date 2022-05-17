49371b4cc327552613aba664025ff670a7fed1ef
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-17 10:45:30 +0600

    modal handle

--- src/pages/Services/BookingModal.js
+++ src/pages/Services/BookingModal.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import Swal from 'sweetalert2';
 import auth from '../../Firebase/firebase.init';
 
 const BookingModal = ({ getService, setGetService }) => {
     const [user] = useAuthState(auth)
+
     const handleBooking = (event) => {
         event.preventDefault()
-        console.log('clicked');
+        const bookingInfo = {
+            serviceName : getService?.name,
+            available : getService?.available ,
+            userName : user?.displayName,
+            email : user?.email ,
+            address: event.target.address.value,
+            phone: event.target.phone.value
+        }
+        if (bookingInfo.address === '' ) {
+            Swal.fire({
+                text: "Provide Original Address",
+                icon: 'error',
+                confirmButtonText: 'Okay'
+            })
+            return
+        }
+        if (bookingInfo.phone === '' ) {
+            Swal.fire({
+                text:'Provide Phone Number',
+                icon: 'error',
+                confirmButtonText: 'Okay'
+            })
+            return
+        }
+        
+        else{
+            setGetService(null)
+        }
+        
+        console.log(bookingInfo);
+
+
+
     }
-    console.log(getService);
 
 
 
@@ -22,7 +55,7 @@ const BookingModal = ({ getService, setGetService }) => {
                     <p className="py-2 text-lg">Available:  {getService?.available}</p>
                     <div className='text-center '>
                         <form onSubmit={handleBooking} className='space-y-4 pt-8 '>
-                            <input type="text" disabled value={user?.displayName || ''} name='Ghost' className="input input-bordered w-full max-w-md text-lg" />
+                            <input type="text" disabled value={user?.displayName || ''} name='userName' className="input input-bordered w-full max-w-md text-lg" />
                             <input type="email" disabled value={user?.email || ''} name='email' className="input input-bordered w-full max-w-md text-lg" />
                             <textarea  rows={2} type="text" placeholder='Your Address' name='address' className=" input-bordered w-full textarea max-w-md text-lg " />
                             <input type="number" placeholder="Phone Number" name='phone' className="input input-bordered w-full max-w-md text-lg" />
